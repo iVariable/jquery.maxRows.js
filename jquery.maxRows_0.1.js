53544d8cf0795ddee99a973400b917986716f86b
iVariable <iVariable@gmail.com>
2010-11-10 11:36:07 +0500

    Fixed min height bug

--- jquery.maxRows_0.1.js
+++ jquery.maxRows_0.1.js
@@ -6,21 +6,13 @@
  * Copyright (c) 2010 Vladimir Savenkov <iVariable@gmail.com>
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) licenses.
  * 
- * Version: 0.1
+ * Version: 0.1b
  *
  * Demo and documentation: (LINK)
  */
 (function($){
 	
 	var helpers = {
-		
-		checkAndMake: function( height ){
-			var $this = $(this);
-			var maxHeight = $this.data('maxHeight');						 
-			while( $this.attr('scrollHeight') > maxHeight ){
-				$this.val( $this.val().slice(0,-1) );								
-			}									
-		},
 		
 		determineMaxRowsByHeight: function(){
 						
@@ -33,12 +25,17 @@
 	}
 	
 	var methods = {
-		getMaxRows: function(){
-			var result = {
-				'maxRows': this.data('maxRows'),
-				'maxHeight': this.data('maxHeight')
-			};	
-			return (result.maxHeight == null)?null:result;
+		getMaxRows: function(){					
+			return this.map(function(){
+				$this = $(this);
+				var result = {
+					'maxRows': $this.data('maxRows'),
+					'maxHeight': $this.data('maxHeight'),
+					'clone': $this.data('clone')
+				};
+				return (result.maxHeight == null)?null:result;
+			});
+			
 		},
 		
 		setMaxRows: function( rows ){			
@@ -46,8 +43,9 @@
 			if( maxRows == 'NaN' ) maxRows = -1;
 			
 			return this.each(function(){		
-				var $this = $(this);
-				if( $this.attr('type') != 'textarea' ) return;				
+				var $this = textarea = $(this);
+				if( $this.attr('type') != 'textarea' ) return;
+								
 				var maxHeight = 0;
 				var maxRowsThis = maxRows;
 				if (maxRowsThis == -1) {
@@ -56,21 +54,57 @@
 				}else{
 					maxHeight = helpers.determineMaxHeightByRows.apply( $this, [maxRowsThis] );
 				};	
-							
-				if( maxRowsThis == 0 ){ // Remove row limit					
-					$this.unbind( '.maxrows' );
-					$this.removeData( 'maxRows' ).removeData( 'maxHeight' );
-				}else{	
+				
+				$this.unbind( '.maxrows' );
+				$this.removeData( 'maxRows' ).removeData( 'maxHeight' ).removeData( 'clone' );
+				
+				if( maxRowsThis != 0 ){	
 					
-					$this.bind( 'keydown.maxrows', function( e ){																		
-						helpers.checkAndMake.apply( this, arguments );						
-					});
-					$this.bind( 'keyup.maxrows', function( e ){																		
-						helpers.checkAndMake.apply( this, arguments );						
-					});
+					// Need clone of textarea, hidden off screen:
+	                var clone = (function(){
+	                    
+	                    // Properties which may effect space taken up by chracters:
+	                    var props = ['height','width','lineHeight','textDecoration','letterSpacing'],
+	                        propOb = {};
+	                        
+	                    // Create object of styles to apply:
+	                    $.each(props, function(i, prop){
+	                        propOb[prop] = textarea.css(prop);
+	                    });
+	                    
+	                    // Clone the actual textarea removing unique properties
+	                    // and insert before original textarea:
+	                    return textarea.clone().removeAttr('id').removeAttr('name').css({
+	                        position: 'absolute',
+	                        top: 0,
+	                        left: -9999,
+							resize:'none',
+							'overflow-y':'hidden'
+	                    }).css(propOb).attr('tabIndex','-1').insertBefore(textarea);
+						
+	                })()
+					
+					var checkSize = function() {						
+						$this = $(this);						
+	                    // Prepare the clone:
+	                    clone.height(0).val($this.val()).scrollTop(10000);						
+	                    // Find the height of text:
+	                    var textHeight = clone.scrollTop();
+						var i = 0;
+						while( textHeight > maxHeight ){
+							$this.val( $this.val().slice(0,-1) );
+							clone.height(0).val($this.val()).scrollTop(10000);
+							textHeight = clone.scrollTop();
+							if ( ($this.val() == '') )break;							
+						}
+	                };
+
+					
+					$this.bind( 'keydown.maxrows change.maxrows keyup.maxrows', checkSize );
 					
 					$this.data('maxRows', maxRowsThis);
-					$this.data('maxHeight', maxHeight);					
+					$this.data('maxHeight', maxHeight);
+					$this.data('clone', clone);					
 				}				
 				
 			});
